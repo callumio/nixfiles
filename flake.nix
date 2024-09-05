152ce6cfd2d91c5e8e73f4cc0f6192ea6dbbd117
Callum Leslie <git@cleslie.uk>
2024-09-05 10:37:59 +0100

    remove fup

--- flake.nix
+++ flake.nix
@@ -1,6 +1,91 @@
 {
   description = "C's Nix-Config";
 
+  outputs = {
+    self,
+    nixpkgs,
+    flake-parts,
+    ...
+  } @ inputs: let
+    mods = import ./modules;
+    mkLinuxSystem = mod:
+      nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs;};
+        modules =
+          [
+            inputs.home-manager.nixosModules.home-manager
+            inputs.stylix.nixosModules.stylix
+            inputs.agenix.nixosModules.default
+            {
+              nixpkgs.config.allowUnfree = true;
+              nixpkgs.overlays = [self.overlays.default];
+            }
+            mod
+          ]
+          ++ mods.sharedModules;
+      };
+  in
+    flake-parts.lib.mkFlake {inherit self inputs;} {
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+        inputs.pre-commit-hooks.flakeModule
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      systems = import inputs.systems;
+
+      flake = {
+        inherit (mods) homeManagerModules nixosModules;
+        # TODO: use ./hosts/
+        nixosConfigurations = {
+          artemis = mkLinuxSystem ./hosts/artemis;
+          hermes = mkLinuxSystem ./hosts/hermes;
+        };
+      };
+
+      perSystem = {
+        config,
+        pkgs,
+        final,
+        system,
+        inputs',
+        #self',
+        ...
+      }: {
+        _module.args.pkgs = inputs'.nixpkgs.legacyPackages.extend self.overlays.default;
+        overlayAttrs = config.packages // {unstable = inputs.unstable.legacyPackages.${system};};
+
+        pre-commit.check.enable = false;
+        pre-commit.settings.hooks.alejandra.enable = true;
+
+        treefmt.config = {
+          projectRootFile = "flake.nix";
+          programs.alejandra.enable = true;
+        };
+
+        devShells.default = final.mkShell {
+          meta.description = "Default dev shell";
+          inputsFrom = [config.pre-commit.devShell config.treefmt.build.devShell];
+          packages = with final; [just git nixvim cachix jq devour-flake agenix deadnix];
+        };
+
+        apps = nixpkgs.lib.mapAttrs' (name: value: nixpkgs.lib.nameValuePair ("deploy-" + name) value) (inputs.nixinate.nixinate.${system} self).nixinate;
+
+        packages = {
+          nixvim = inputs.nixvim.packages.${system}.default;
+          agenix = inputs.agenix.packages.${system}.default;
+          vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+          devour-flake = pkgs.callPackage inputs.devour-flake {};
+          jellyfin-ffmpeg = pkgs.jellyfin-ffmpeg.override {
+            ffmpeg_6-full = pkgs.ffmpeg_6-full.override {
+              withMfx = false;
+              withVpl = true;
+            };
+          };
+        };
+      };
+    };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -27,8 +112,6 @@
       # i don't need darwin!!!
       inputs.darwin.follows = "";
     };
-
-    utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
@@ -59,100 +142,14 @@
         flake-compat.follows = "flake-compat";
       };
     };
+
     flake-compat.url = "github:edolstra/flake-compat";
     flake-parts.url = "github:hercules-ci/flake-parts";
+
     systems.url = "github:nix-systems/default";
     #systems.url = "github:nix-systems/default-linux";
     #systems.url = "github:nix-systems/x86_64-linux";
+
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
-
-  outputs = {
-    self,
-    disko,
-    nixpkgs,
-    flake-parts,
-    nixinate,
-    utils,
-    home-manager,
-    ...
-  } @ inputs: let
-    mods = import ./modules {inherit utils;};
-    mkLinuxSystem = mod:
-      nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
-        modules =
-          [
-            inputs.home-manager.nixosModules.home-manager
-            inputs.stylix.nixosModules.stylix
-            inputs.agenix.nixosModules.default
-            {
-              nixpkgs.config.allowUnfree = true;
-              nixpkgs.overlays = [self.overlays.default];
-            }
-            mod
-          ]
-          ++ mods.sharedModules;
-      };
-  in
-    flake-parts.lib.mkFlake {inherit self inputs;} {
-      imports = [
-        inputs.flake-parts.flakeModules.easyOverlay
-        inputs.pre-commit-hooks.flakeModule
-        inputs.treefmt-nix.flakeModule
-      ];
-
-      systems = import inputs.systems;
-
-      flake = {
-        inherit (mods) homeManagerModules nixosModules;
-        nixosConfigurations = {
-          artemis = mkLinuxSystem ./hosts/artemis;
-          hermes = mkLinuxSystem ./hosts/hermes;
-        };
-        #nixosConfigurations.artemis = inputs.nixpkgs.lib.nixosSystem {};
-      };
-
-      perSystem = {
-        config,
-        pkgs,
-        final,
-        system,
-        inputs',
-        self',
-        ...
-      }: {
-        _module.args.pkgs = inputs'.nixpkgs.legacyPackages.extend self.overlays.default;
-        overlayAttrs = config.packages // {unstable = inputs.unstable.legacyPackages.${system};};
-
-        pre-commit.check.enable = false;
-        pre-commit.settings.hooks.alejandra.enable = true;
-
-        treefmt.config = {
-          projectRootFile = "flake.nix";
-          programs.alejandra.enable = true;
-        };
-
-        devShells.default = final.mkShell {
-          meta.description = "Default dev shell";
-          inputsFrom = [config.pre-commit.devShell config.treefmt.build.devShell];
-          packages = with final; [just git nixvim cachix jq devour-flake agenix deadnix];
-        };
-
-        apps = nixpkgs.lib.mapAttrs' (name: value: nixpkgs.lib.nameValuePair ("deploy-" + name) value) (nixinate.nixinate.${system} self).nixinate;
-
-        packages = {
-          nixvim = inputs.nixvim.packages.${system}.default;
-          agenix = inputs.agenix.packages.${system}.default;
-          vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
-          devour-flake = pkgs.callPackage inputs.devour-flake {};
-          jellyfin-ffmpeg = pkgs.jellyfin-ffmpeg.override {
-            ffmpeg_6-full = pkgs.ffmpeg_6-full.override {
-              withMfx = false;
-              withVpl = true;
-            };
-          };
-        };
-      };
-    };
 }
--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,21 @@
-{utils, ...}: let
-  nixosModules = utils.lib.exportModules [
+let
+  exportModules = args:
+    builtins.listToAttrs (map (arg: {
+        name = let
+          str = baseNameOf arg;
+          suffix = ".nix";
+          sufLen = builtins.stringLength suffix;
+          sLen = builtins.stringLength str;
+        in
+          if sufLen <= sLen && suffix == builtins.substring (sLen - sufLen) sufLen str
+          then builtins.substring 0 (sLen - sufLen) str
+          else str;
+
+        value = import arg;
+      })
+      args);
+
+  nixosModules = exportModules [
     ./nix.nix
     ./pinentry-fix.nix
     ./hm.nix
@@ -9,7 +25,7 @@
     ./tailscale.nix
     ./secret.nix
   ];
-  homeManagerModules = utils.lib.exportModules [
+  homeManagerModules = exportModules [
     ./trayscale.nix
   ];
   sharedModules = with nixosModules; [
