098183449742c5ad3f803bb16a3e4f9c29a1608d
Callum Leslie <git@cleslie.uk>
2025-09-17 08:17:40 +0100

    add pager and private commits for jujutsu

--- home/c/programs/jj/default.nix
+++ home/c/programs/jj/default.nix
@@ -10,12 +10,25 @@
         behavior = "own";
         backend = "gpg";
       };
+
+      ui = {
+        pager = "delta";
+        diff-formatter = ":git";
+      };
+      git = {
+        private-commits = "description(glob:'wip:*') | description(glob:'private:*') | description(glob:'fixup*:*')";
+      };
+
       "--scope" = [
         {
           "--when".repositories = ["~/repos/projects.cs.nott.ac.uk"];
           user = {
             email = "psycl6@nottingham.ac.uk";
           };
+        }
+        {
+          "--when".commands = ["status"];
+          ui.paginate = "never";
         }
       ];
     };
